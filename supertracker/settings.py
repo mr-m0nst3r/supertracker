f4b23faa8fd7f1e69b4941414c79ce7b4014fe01
mr-m0nst3r <songxinlei@gmail.com>
2022-03-11 10:39:52 +0800

    add requirements

--- supertracker/settings.py
+++ supertracker/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.core.management.utils import get_random_secret_key
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
